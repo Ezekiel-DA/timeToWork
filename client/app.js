e8cdca01ea58ef81242f4726564f0beadb661406
Nicolas Lefebvre <nicolas.lefebvre.lp@gmail.com>
2016-06-24 15:27:46 -0400

    show current travel time on top

--- client/app.js
+++ client/app.js
@@ -5,36 +5,40 @@ import moment from 'moment';
 import * as services from './services'; 
 
 function sToM(s) {
-    return (s / 60);
+    return (s / 60)
+}
+
+function numSortReverse(a, b) {
+    return Number(b) - Number(a)
 }
 
 function rawDataToSingleArray(raw) {
     return Object.keys(raw.values).map(h => Object.keys(raw.values[h]).map(m => {
-        var time = moment();
-        time.hours(h);
-        time.minutes(m);
+        let time = moment()
+        time.hours(h)
+        time.minutes(m)
         return {
             date: time.toDate(),
             value: time.hours() < 12 ? sToM(raw.values[h][m].toWork) : (time.hours() < 13 ? null : sToM(raw.values[h][m].fromWork)),
             daystamp: raw.daystamp
-        };
+        }
     })).reduce(function (prev, cur) {
-        prev = prev.concat(cur);
-        return prev;
-    }, []);
+        prev = prev.concat(cur)
+        return prev
+    }, [])
 }
 
 function drawAll(allRawData, target) {
-    var allData = allRawData.filter(rawData => rawData.values).map(rawDataToSingleArray);
+    let allData = allRawData.filter(rawData => rawData.values).map(rawDataToSingleArray)
     
     if (!allData.length) {
-        return; // nothing to do, no data at all in input !
+        return // nothing to do, no data at all in input !
     }
 
-    var timeMarkers = [
+    let timeMarkers = [
         {date: moment().hours(8).minutes(0).toDate()},
         {date: moment().hours(17).minutes(0).toDate()}
-    ];
+    ]
     MG.data_graphic({
         data: allData,
         'title': `${moment(allRawData[0].daystamp).format('dddd MMMM Do YYYY')} to ${moment(allRawData[allRawData.length-1].daystamp).format('dddd MMMM Do YYYY')}`,
@@ -43,14 +47,14 @@ function drawAll(allRawData, target) {
         'y_label': 'mins', 'animate_on_load': true, 'missing_is_hidden': true, 'min_y': 30, 'show_secondary_x_label': false, 'decimals': 0,
         'y_extended_ticks': true, 'min_x': moment().hours(6).minutes(0).toDate(), 'max_x': moment().hours(20).minutes(0).toDate(),
         'legend': allData.map(data => moment(data[0].daystamp).format('ddd')), 'x_rollover_format': '%H:%M ', 'legend_target': target.legend
-    });
+    })
 }
 
 function getData(date) {
     return fetch('/api/data/' + date.toDate().toISOString(), { credentials: 'include' })
     .then(function (res) {
-        return res.json();
-    });
+        return res.json()
+    })
 }
 
 // Create divs for the content and legends, returning a new one for each call to the generator and adding horizontal
@@ -58,39 +62,55 @@ function getData(date) {
 // strings corresponding to the CSS IDs of the inserted elements.
 // The input to the generator is the original div to add ourselves under.
 function* TargetDivGenerator(root) {
-    let count = 0;
+    let count = 0
     while(1) {
-        let targetDiv = document.createElement('div');
-        let id = `ttw-graph-${count++}`;
-        targetDiv.id = id;
-        root.appendChild(targetDiv);
-        let legendTargetDiv = document.createElement('div');
-        let legendId = `ttw-graph-legend-${count}`;
-        legendTargetDiv.id = legendId;
-        legendTargetDiv.style.textAlign = 'center';
-        root.appendChild(legendTargetDiv);
+        let targetDiv = document.createElement('div')
+          , legendTargetDiv = document.createElement('div')
+          , hr = document.createElement('hr')
+          , id = `ttw-graph-${count++}`
+          , legendId = `ttw-graph-legend-${count}`
+          
+        targetDiv.id = id
+        legendTargetDiv.id = legendId
+        legendTargetDiv.style.textAlign = 'center'        
+        root.appendChild(targetDiv)
+        root.appendChild(legendTargetDiv)
+        
         yield {
             graph: `#${id}`,
             legend: `#${legendId}`
-        };
-        let hr = document.createElement('hr');
-        root.appendChild(hr);
+        }        
+        root.appendChild(hr)
     }
 }
 
-var targetDivMaker = TargetDivGenerator(document.getElementById('ttw-graphs'));
+var targetDivMaker = TargetDivGenerator(document.getElementById('ttw-graphs'))
 
-var today = moment().startOf('day');
+var today = moment().startOf('day')
 if (today.isoWeekday() === 6 || today.isoWeekday() === 7) { // jump back to friday if weekend
-    today.isoWeekday(5);
-
+    today.isoWeekday(5)
 }
-var weeks = services.buildWeeks(today, 4);
-weeks.reverse().forEach(week => {
+
+services.buildWeeks(today, 4).reverse().forEach(week => {
     Promise.all(week.map(getData))
     .then(weekData => {
         if (weekData.some(day => day.values)) { // draw only if at least one day has actual data to show
-            drawAll(weekData, targetDivMaker.next().value);
+            drawAll(weekData, targetDivMaker.next().value)
+            
+            // figure out the latest time estimate and display it at the top
+            var travelTimeNow = weekData.filter(day => moment(day.daystamp).isSame(today, 'day'))    
+            .map(day => {
+                let hour = Object.keys(day.values).sort(numSortReverse)[0]
+                  , minute = Object.keys(day.values[hour]).sort(numSortReverse)[0]
+                
+                return { hour, minute, val : day.values[hour][minute] }
+            })[0];
+            
+            if (travelTimeNow) {
+                let from = travelTimeNow.hour > 12
+                  , text = `Current time ${from ? 'from' : 'to'} work is ${Math.round(sToM(from ? travelTimeNow.val.fromWork: travelTimeNow.val.toWork))} minutes`
+                document.querySelector('div#current-time span').textContent = text
+            }
         }
     });
 });
--- client/services.js
+++ client/services.js
@@ -1,23 +1,6 @@
 import moment from 'moment';
 import 'moment-range';
 
-// Build the list of n valid dates ('today' included) to display from the 'today' input, in chronological order
-function buildMoments(today, n = 5) {
-    let t = moment(today);
-    let ret = [];
-    ret.push(moment(t));
-
-    while (ret.length < n) {
-        t.subtract(1, 'days');
-        if (t.isoWeekday() === 6 || t.isoWeekday() === 7) {
-            continue;
-        }
-        ret.push(moment(t));
-    }
-
-    return ret.reverse();
-}
-
 // Build a list of lists corresponding to moments (dates) for n weeks before today, in chronological order
 // In other words : if today is Wednesday Jun 22 and n is 3, the expected output is a bunch of moments corresponding to:
 // [ [6/6, 6/7, 6/8, 6/9, 6/10], [6/13, 6/14, 6/15, 6/16, 6/17], [6/20, 6/21, 6/22] ]
@@ -29,6 +12,5 @@ function buildWeeks(today, n = 3) {
 }
 
 export {
-    buildMoments,
     buildWeeks
 };

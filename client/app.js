d7d7edb0269072bcade942cd7e490e41ce364fb6
Nicolas LEFEBVRE <nicolas.lefebvre.lp@gmail.com>
2016-06-26 19:43:32 -0400

    fix title alignemt (forked and patched mozilla/metrics-graphics)

--- client/app.js
+++ client/app.js
@@ -42,8 +42,8 @@ function drawAll(allRawData, target) {
     MG.data_graphic({
         data: allData,
         'title': `${moment(allRawData[0].daystamp).format('dddd MMMM Do YYYY')} to ${moment(allRawData[allRawData.length-1].daystamp).format('dddd MMMM Do YYYY')}`,
-        'target': target.graph, 'full_width': true, 'height': 500, 'left': 100,
-        'markers': timeMarkers, 'x_extended_ticks': true, 'xax_count': 10, 'yax_count': 10,
+        'target': target.graph, 'full_width': true, 'height': 500, 'left': 100, 'center_title_full_width': true,
+        'markers': timeMarkers, 'x_extended_ticks': false, 'xax_count': 10, 'yax_count': 10,
         'y_label': 'mins', 'animate_on_load': true, 'missing_is_hidden': true, 'min_y': 30, 'show_secondary_x_label': false, 'decimals': 0,
         'y_extended_ticks': true, 'min_x': moment().hours(6).minutes(0).toDate(), 'max_x': moment().hours(20).minutes(0).toDate(),
         'legend': allData.map(data => moment(data[0].daystamp).format('ddd')), 'x_rollover_format': '%H:%M ', 'legend_target': target.legend
@@ -100,7 +100,7 @@ services.buildWeeks(today, 4).reverse().forEach(week => {
             // figure out the latest time estimate and display it at the top
             var travelTimeNow = weekData.filter(day => moment(day.daystamp).isSame(today, 'day'))    
             .map(day => {
-                let hour = Object.keys(day.values).sort(numSortReverse)[0]
+                let hour = Object.keys(day.values).sort(numSortReverse)[0]  
                   , minute = Object.keys(day.values[hour]).sort(numSortReverse)[0]
                 
                 return { hour, minute, val : day.values[hour][minute] }

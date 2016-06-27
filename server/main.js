7e57975fae5668b150def428ad9d7795ba0348b2
Nicolas Lefebvre <nicolas.lefebvre.lp@gmail.com>
2016-06-27 14:15:58 -0400

    retry initial mongoDB connection, with exponential backoff

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@
 (function () {
     'use strict';
 
+    let Promise = require('bluebird');
     let request = require('request-promise');
     let httpStatus = require('http-status');
     let process = require('process');
@@ -16,13 +17,23 @@
     app.disable('x-powered-by');
     app.use(express.static('client/static'));
     app.listen(8000);
+    
+    // generate a delay (in milliseconds) to back off before the next connection attempt to MongoDB
+    // warning: input in SECONDS, output in MILLISECONDS
+    function* exponentialBackoffGenerator(maxRetries = 10, firstDelayInS = 3, maxDelayInS = 10 * 60) {
+        let tries = 0
+        while (tries < maxRetries) {
+            yield Math.min(maxDelayInS, Math.pow(firstDelayInS, tries++)) * 1000
+        }
+    }
 
     // connect to MongoDB
     let mongodbUrl = process.env['TTW_MONGODB_URL'];
     if (!mongodbUrl) {
         throw new Error('Missing MongoDB URL setting. Did you remember to set TTW_MONGODB_URL to mongodb://<host>:<port>/<db> ?');
     }
-    mongodb.MongoClient.connect(mongodbUrl).then(db => {
+
+    function onDBConnected(db) {
         console.log('connected to MongoDB');
         let collection = db.collection('datapoints');
 
@@ -39,7 +50,7 @@
 
             collection.findOne({ daystamp: reqDate.startOf('day').toDate() })
                 .then(data => {
-                    data = data || {daystamp: req.params.day};
+                    data = data || { daystamp: req.params.day };
                     res.send(data);
                 })
                 .catch(err => {
@@ -47,9 +58,19 @@
                     res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
                 });
         });
+    }
 
-    }).catch(err => {
-        console.log('MongoDB connection error: ', err);
-        process.exit(1);
-    });
+    let eb = exponentialBackoffGenerator()
+
+    mongodb.MongoClient.connect(mongodbUrl)
+    .then(onDBConnected)
+    .catch(function retryConnectionOnError(err) {
+        console.log('MongoDB connection error:', err)
+        let waitBeforeNextAttempt = eb.next();
+        if (waitBeforeNextAttempt.done) {
+            console.log('FATAL ERROR: too many failed connection attempts to MongoDB. Giving up...')
+            process.exit(1)
+        }
+        return Promise.delay(eb.next().value).then(mongodb.MongoClient.connect.bind(mongodb.MongoClient, mongodbUrl)).then(onDBConnected).catch(retryConnectionOnError)
+    })
 })();

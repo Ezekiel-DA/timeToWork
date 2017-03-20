dbd00c940d982b45e7847e685dbb94490bd10433
Nicolas LEFEBVRE <nicolas.lefebvre.nlv@gmail.com>
2017-03-19 22:05:23 -0400

    convert to standardJS

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,65 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var livereload = require('gulp-livereload');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var buffer = require('vinyl-buffer');
-var source = require('vinyl-source-stream');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var gulpif = require('gulp-if');
-var tape = require('gulp-tape');
-var tapColorize = require('tap-colorize');
-var babel = require('gulp-babel');
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon')
+var livereload = require('gulp-livereload')
+var browserify = require('browserify')
+var babelify = require('babelify')
+var buffer = require('vinyl-buffer')
+var source = require('vinyl-source-stream')
+var uglify = require('gulp-uglify')
+var sourcemaps = require('gulp-sourcemaps')
+var gulpif = require('gulp-if')
+var tape = require('gulp-tape')
+var tapColorize = require('tap-colorize')
+var babel = require('gulp-babel')
 
-var production = process.env.NODE_ENV === 'production';
+var production = process.env.NODE_ENV === 'production'
 
 gulp.task('start', function () {
-    nodemon({
-        script: './server/main.js',
-        watch: ['server'],
-        ignore: ['gulpfile.js'],
-        env: { 'NODE_ENV': process.env.NODE_ENV || 'development' }
-    });
-});
+  nodemon({
+    script: './server/main.js',
+    watch: ['server'],
+    ignore: ['gulpfile.js'],
+    env: { 'NODE_ENV': process.env.NODE_ENV || 'development' }
+  })
+})
 
 gulp.task('browserify', function () {
-    return browserify({ entries: 'client/app.js', debug: !production })
+  return browserify({ entries: 'client/app.js', debug: !production })
         .transform(babelify)
         .bundle()
         .pipe(source('app_bundle.js'))
         .pipe(buffer())
-        //.pipe(sourcemaps.init({ loadMaps: true }))
+        // .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(gulpif(production, uglify({ mangle: true })))
-        //.pipe(sourcemaps.write('.'))
+        // .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('client/static/build'))
-        .pipe(livereload());
-});
+        .pipe(livereload())
+})
 
 gulp.task('watch-static', function () {
-    gulp.watch(['client/static/*.html'], function () {
-        gulp.src(['client/static/*.html'])
-            .pipe(livereload());
-    });
-});
+  gulp.watch(['client/static/*.html'], function () {
+    gulp.src(['client/static/*.html'])
+            .pipe(livereload())
+  })
+})
 
 gulp.task('watch-app', function () {
-    gulp.watch(['client/*.js'], ['browserify']);
-});
+  gulp.watch(['client/*.js'], ['browserify'])
+})
 
-gulp.task('test', function() {
-    return gulp.src('tests/*.js')
+gulp.task('test', function () {
+  return gulp.src('tests/*.js')
         .pipe(sourcemaps.init())
         .pipe(babel({presets: ['es2015']}))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('tests/build'))
         .pipe(tape({
-            reporter: tapColorize()
-        }));
-});
+          reporter: tapColorize()
+        }))
+})
 
 gulp.task('default', ['browserify', 'start', 'watch-static', 'watch-app'], function () {
-    livereload.listen();
-});
+  livereload.listen()
+})
--- server/dataCollection.js
+++ server/dataCollection.js
@@ -1,84 +1,83 @@
 (function () {
-    'use strict';
+  'use strict'
 
-    var moment = require('moment');
-    var request = require('request-promise');
-    var process = require('process');
+  var moment = require('moment')
+  var request = require('request-promise')
+  var process = require('process')
 
     // Google Distance Matrix API setup
-    var places;
-    try {
-        places = JSON.parse(process.env['TTW_PLACES_JSON_STRING']);
-        if (!places) {
-            throw new Error('Places environment variable parsed as JSON but resulted in an empty object.');
-        }
-    } catch (err) {
-        console.log('Failed to get home/work place settings. Did you remember to set TTW_PLACES_JSON_STRING to {"home": "<address>", "work": "<address">} ?');
-        throw err;
+  var places
+  try {
+    places = JSON.parse(process.env['TTW_PLACES_JSON_STRING'])
+    if (!places) {
+      throw new Error('Places environment variable parsed as JSON but resulted in an empty object.')
     }
+  } catch (err) {
+    console.log('Failed to get home/work place settings. Did you remember to set TTW_PLACES_JSON_STRING to {"home": "<address>", "work": "<address">} ?')
+    throw err
+  }
 
+  let apiKey = process.env['TTW_GOOGLE_DISTANCE_MATRIX_API_KEY']
+  if (!apiKey) {
+    throw new Error('Missing API key for Google Distance Matrix API. Did you remember to set TTW_GOOGLE_DISTANCE_MATRIX_API_KEY ?')
+  }
 
-    let apiKey = process.env['TTW_GOOGLE_DISTANCE_MATRIX_API_KEY'];
-    if (!apiKey) {
-        throw new Error('Missing API key for Google Distance Matrix API. Did you remember to set TTW_GOOGLE_DISTANCE_MATRIX_API_KEY ?');
-    }
-
-    let baseUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json';
-    let options = 'departure_time=now&mode=driving&alternatives=true&units=metric&traffic_model=best_guess';
-    let urlToWork = `${baseUrl}?origins=${places.home}&destinations=${places.work}&key=${apiKey}&${options}`;
-    let urlFromWork = `${baseUrl}?origins=${places.work}&destinations=${places.home}&key=${apiKey}&${options}`;
+  let baseUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+  let options = 'departure_time=now&mode=driving&alternatives=true&units=metric&traffic_model=best_guess'
+  let urlToWork = `${baseUrl}?origins=${places.home}&destinations=${places.work}&key=${apiKey}&${options}`
+  let urlFromWork = `${baseUrl}?origins=${places.work}&destinations=${places.home}&key=${apiKey}&${options}`
 
     // query Google Distance Matrix API
-    function queryTravelTime() {
-        return Promise.all([request(urlToWork, { json: true }), request(urlFromWork, { json: true })])
+  function queryTravelTime () {
+    return Promise.all([request(urlToWork, { json: true }), request(urlFromWork, { json: true })])
             .then(resArray => {
-                let [toWork, fromWork] = resArray.map(res => res.rows[0].elements[0].duration_in_traffic.value);
-                return { toWork, fromWork };
-            });
-    }
+              let [toWork, fromWork] = resArray.map(res => res.rows[0].elements[0].duration_in_traffic.value)
+              return { toWork, fromWork }
+            })
+  }
 
     // query Google Distance Matrix API and store the results in MongoDB
-    function getTravelTimeAndStoreInDB(collection) {
-        queryTravelTime().then(timeInfo => {
-            let now = moment();
-            let data = {};
-            data[`values.${now.hours()}.${now.minutes()}`] = timeInfo;
+  function getTravelTimeAndStoreInDB (collection) {
+    queryTravelTime().then(timeInfo => {
+      let now = moment()
+      let data = {}
+      data[`values.${now.hours()}.${now.minutes()}`] = timeInfo
 
-            collection.updateOne(
+      collection.updateOne(
                 { daystamp: now.startOf('day').toDate() },
                 { $set: data },
                 { upsert: true }
-            );
-        });
-    }
+            )
+    })
+  }
 
     // returns the delay in milliseconds to wait before the next API call, depending on the current
     // time and various rules designed to avoid logging data at uninteresting times and going over our API quota
-    function nextAPICallDelay(fromTime) {
-        var h = fromTime.hour();
-        if ((h >= 6 && h < 10) || (h >= 15 && h < 19)) { // rush hour; log more often
-            return 1000 * 60 * 2;
-        }
-        return 1000 * 60 * 10;
+  function nextAPICallDelay (fromTime) {
+    var h = fromTime.hour()
+    if ((h >= 6 && h < 10) || (h >= 15 && h < 19)) { // rush hour; log more often
+      return 1000 * 60 * 2
     }
+    return 1000 * 60 * 10
+  }
 
     // is it ok to skip data collection entirely ? (weekends, nights)
-    function canSkipDataCollection(fromTime) {
-        if (fromTime.isoWeekday() === 6 || fromTime.isoWeekday() === 7) { // week end : skip
-            return true;
-        }
-        if (fromTime.hour() >= 20 || fromTime.hour() < 6) { // night
-            return true;
-        }
-        return false;
+  function canSkipDataCollection (fromTime) {
+    if (fromTime.isoWeekday() === 6 || fromTime.isoWeekday() === 7) { // week end : skip
+      return true
     }
+    if (fromTime.hour() >= 20 || fromTime.hour() < 6) { // night
+      return true
+    }
+    return false
+  }
 
-    exports.start = function (collection) {
-        setTimeout(function callAPIAndReschedule() {
-            if (!canSkipDataCollection(moment())) {
-                getTravelTimeAndStoreInDB(collection);
-            }
-            setTimeout(callAPIAndReschedule, nextAPICallDelay(moment()));
-        }, nextAPICallDelay(moment()));
-    };
-})();
+  exports.start = function (collection) {
+    setTimeout(function callAPIAndReschedule () {
+      if (!canSkipDataCollection(moment())) {
+        getTravelTimeAndStoreInDB(collection)
+      }
+      setTimeout(callAPIAndReschedule, nextAPICallDelay(moment()))
+    }, nextAPICallDelay(moment()))
+  }
+})()
